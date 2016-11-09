6092f8471f2222bfae1f9757246be30e070b457b
ionakathryn <ionakathryn123@googlemail.com>
2016-11-09 00:22:39 +0000

    Create initials.c

---
+++ pset2/initials.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <locale.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+    string s = GetString();
+    int i = 0;
+    {
+        printf ("%c", toupper (s[0]));
+    }
+    
+        for ( i = 0; i < strlen(s); i++)
+        {
+            if (s[i] == ' ')
+            {
+                printf ("%c", toupper (s[i + 1]));
+            }
+                }
+
+        {
+            printf ("\n");
+        }
+    
+}
